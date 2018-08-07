7d05773f928cb84f5c0b8b7db76e38355ce2d2f3
jiveace <straightedgescotsman@gmail.com>
2018-08-07 10:57:43 +0100

    Adding SQL seed, and accessing data via JPA Query methods

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package guru.springframework.controllers;
 
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndex(Model model){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("TSP");
+
+        System.out.println("Cat ID is:" + categoryOptional.get().getId());
+        System.out.println("UOM ID is:" + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO category (description) VALUES ('American');
+INSERT INTO category (description) VALUES ('Italian');
+INSERT INTO category (description) VALUES ('Maxican');
+INSERT INTO category (description) VALUES ('Fast Food');
+
+INSERT INTO unit_of_measure (description) VALUES('TSP');
+INSERT INTO unit_of_measure (description) VALUES('TBSP');
+INSERT INTO unit_of_measure (description) VALUES('Cup');
+INSERT INTO unit_of_measure (description) VALUES('Pinch');
+INSERT INTO unit_of_measure (description) VALUES('Ounce');
