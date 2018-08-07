133fb0dcd33b810dc62858ae77a307766af55804
jiveace <straightedgescotsman@gmail.com>
2018-08-07 13:37:19 +0100

    Refactoring project to use Project Lombok.

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -1,7 +1,10 @@
 package guru.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 public class UnitOfMeasure {
 
@@ -10,19 +13,4 @@ public class UnitOfMeasure {
     private String id;
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
